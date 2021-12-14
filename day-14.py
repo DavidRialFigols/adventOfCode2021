6e59ce46d9a5d9bcd9f9d232f9cb11fdfa96774e
David Rial Figols <davidrialfigols@gmail.com>
2021-12-14 10:11:00 +0100

    Update day-14.py

--- day-14.py
+++ day-14.py
@@ -33,51 +33,11 @@ def step(data):
     for pair in pairs:
         data['pairs'][pair][1] -= pairs[pair][1]
         if f"{pair[0]}{data['pairs'][pair][0]}" in data['pairs']:
-            # print(f"{pair[0]}{data['pairs'][pair][0]}")
-            # print(data['pairs'][f"{pair[0]}{data['pairs'][pair][0]}"][1])
             data['pairs'][f"{pair[0]}{data['pairs'][pair][0]}"][1] += pairs[pair][1]
-            # print(pairs)
-            # print(data['pairs'][f"{pair[0]}{data['pairs'][pair][0]}"][1])
-            # if data['pairs'][f"{pair[0]}{data['pairs'][pair][0]}"][1]==pairs[f"{pair[0]}{data['pairs'][pair][0]}"][1]:
-            #     print('son iguals')
-            #     print(pair)
-            #     break
         if f"{data['pairs'][pair][0]}{pair[1]}" in data['pairs']:
-            # print('he entrat =======================================')
-
-            # print(f"{pair[0]}{data['pairs'][pair][0]}")
-            # print(data['pairs'][f"{pair[0]}{data['pairs'][pair][0]}"][1])
             data['pairs'][f"{data['pairs'][pair][0]}{pair[1]}"][1] += pairs[pair][1]
-
-            # print(pairs[pair][1])
-            # print(data['pairs'][f"{pair[0]}{data['pairs'][pair][0]}"][1])
-            # if data['pairs'][f"{data['pairs'][pair][0]}{pair[1]}"][1] == pairs[f"{data['pairs'][pair][0]}{pair[1]}"][1]:
-            #     print('son iguals 2')
-            #     print(pair)
-            #     break
         data['letters'][data['pairs'][pair][0]] += pairs[pair][1]
     return data
-
-    # for i in range(length_template):
-    #     new_template += data['template'][i]
-    #     if count:
-    #         if data['template'][i] in letters_count:
-    #             letters_count[data['template'][i]] += 1
-    #         else:
-    #             letters_count[data['template'][i]] = 1
-
-    #     if i+1 < length_template:
-    #         if f"{data['template'][i]}{data['template'][i+1]}" in data['pairs']:
-    #             new_template += data['pairs'][f"{data['template'][i]}{data['template'][i+1]}"]
-    #             if count:
-    #                 if data['pairs'][f"{data['template'][i]}{data['template'][i+1]}"] in letters_count:
-    #                     letters_count[data['pairs'][f"{data['template'][i]}{data['template'][i+1]}"]] += 1
-    #                 else:
-    #                     letters_count[data['pairs'][f"{data['template'][i]}{data['template'][i+1]}"]] = 1
-    # data['template'] = new_template
-    # if count:
-    #     return letters_count.values()
-    # return data
 
 def process_data_1(data):
     days = 10
