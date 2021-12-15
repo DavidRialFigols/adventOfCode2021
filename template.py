60dae8c4bc8b79502ade95b9f0e2a64a6e94a330
David Rial Figols <davidrialfigols@gmail.com>
2021-12-15 17:58:43 +0100

    Create template.py

---
+++ template.py
@@ -0,0 +1,22 @@
+import copy
+
+def read_file(day):
+    with open(f"data/day-{day}.txt", 'rt') as fin:
+        data = fin.read().rstrip().split("\n")
+    return data
+
+def process_data_1(data):
+    return
+
+def process_data_2(data):
+    return
+
+
+if __name__ == "__main__":
+    day = "15"
+    data = read_file(day)
+    print(data)
+    result_1 = process_data_1(copy.deepcopy(data))
+    print(f"result 1: {result_1}")
+    result_2 = process_data_2(data)
+    print(f"result 2: {result_2}")
